f237a1bc3f8bcb6deb334dd9ffaed9e67861c81f
PAVEL <pashykpavel@gmail.com>
2022-08-24 16:33:08 +0400

    new:button

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Button } from './components/Button';
 
 function App() {
+
+  const onClickPrimary = () => {
+    alert("Hello 1")
+  }
+  const onClickSecondary = () => {
+    alert("Hello 2")
+  }
+  const onClickSecondary2 = () => {
+    alert("Hello 3")
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button text="Primary" onClick={onClickPrimary}/>
+      <Button text="Secondary" onClick={onClickSecondary}/>
+      <Button text="Secondary 2" onClick={onClickSecondary2}/>
     </div>
   );
 }
